64a1fcb83ccb542bdbe48dd52121d5846fd8266f
nuwan <lukynuwan@gmail.com>
2024-05-01 12:20:34 +0530

    upgrade the flutter, dart and dependencies and the changes to work with new versions

--- poc_sl_vehicle_number_plate_text_extraction/lib/main.dart
+++ poc_sl_vehicle_number_plate_text_extraction/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,18 +26,37 @@ class _HomePageState extends State<HomePage> {
 
   void extractTextFromImage(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
-    final textDetector = GoogleMlKit.vision.textDetector();
-    final RecognisedText recognisedText =
-        await textDetector.processImage(inputImage);
-    await textDetector.close();
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    await textRecognizer.close();
 
-    setState(() {
-      _extractedText = recognisedText.text;
-    });
+    // Handle the recognized text as needed
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Recognized Text"),
+          content: SingleChildScrollView(
+            child: Text(recognizedText.text),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
+// Function to handle image picking and text recognition
   Future<void> pickImage() async {
-    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
     if (image != null) {
       extractTextFromImage(image);
     }
